2ce55ec1716486626801a2d6b0ab36c42dd50ce0
Camilo Artunduaga <camiloandresartunduagacaicedo@gmail.com>
2021-03-07 19:27:00 -0500

    filtro terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+//Rutas
+import { APP_ROUTING } from './app.routes'
+
+
+//Servicios
+import { HeroesService } from './services/heroes.service'
+
+
+
+//componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+import { from } from 'rxjs';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { FormsModule } from '@angular/forms';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    HeroesComponent,
+    AboutComponent,
+    HeroeComponent,
+    BuscadorComponent,
+    HeroeTarjetaComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {  HeroesService, Heroe } from '../../services/heroes.service'
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styles: [
+  ]
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Heroe [] = [];
+
+  constructor( private heroesServices: HeroesService,
+               private router: Router ) {
+    
+   }
+
+  ngOnInit() {
+    this.heroes = this.heroesServices.getHeroes();
+    
+  }
+
+  verHeroe( idx: number ){
+    this.router.navigate( ['/heroe', idx] )
+
+  }
+
+
+
+}
+
